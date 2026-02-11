875ab5fff8d039bc7e71443d3887e6cc02e370e1
Egor <mxalk2629@gmail.com>
2026-02-11 15:29:12 +0400

    Сделал менеджер для диалогов

---
+++ Assets/_ProjectRestaurant/UI/_Prefabs/Training/DialogueWindows/Scripts/DialogueManager.cs
@@ -0,0 +1,71 @@
+public class DialogueManager
+{
+    private EndDialogueUI _endDialogue;
+    private StartDialogueUI _startDialogue;
+    private TaskDialogueUI _taskDialogue;
+    private MiniTaskDialogueUI _miniTaskDialogue;
+    
+    //private GetTableDialogue _getTableApple;
+    //private GetTableDialogue _getTableOrange;
+    //private GiveTableDialogue _giveTable;
+    //private CuttingTableDialogue _cuttingTable;
+    //private DistributionDialogue _distribution;
+
+
+    private void StartWedding()
+    {
+        //_startDialogue.Show();
+    }
+    
+    private void StartCookingSalat()
+    {
+        //_taskDialogue.Show();
+    }
+    
+    private void StartTakeApple()
+    {
+        //_miniTaskDialogue.Show();
+        //_getTableApple.Blink();
+    }
+    
+    private void StartBringCuttingTable() 
+    {
+        //_miniTaskDialogue.ChangeText();
+        //_miniTaskDialogue.Show();
+        //_cuttingTable.Blink();
+    }
+    
+    private void StartTakeOrange()
+    {
+        //_miniTaskDialogue.ChangeText();
+        //_miniTaskDialogue.Show();
+        //_getTableOrange.Blink();
+    }
+    
+    // StartBringCuttingTable()
+    
+    private void StartCompliment()
+    {
+        //_taskDialogue.ChangeText();
+        //_taskDialogue.Show();
+    }
+    
+    private void StartWaitOrder()
+    {
+        //_miniTaskDialogue.ChangeText();
+        //_miniTaskDialogue.Show();
+    }
+    
+    private void StartBringDistribution()
+    {
+        //_miniTaskDialogue.ChangeText();
+        //_miniTaskDialogue.Show();
+        //_distribution.Blink();
+    }
+    
+    private void StartGoodbye()
+    {
+        //_endDialogue.Show
+    }
+
+}
